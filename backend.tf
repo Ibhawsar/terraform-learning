60e99ebfab0046a6975f38163fc311af27a1cc7a
Ishika1104 <ishika.bhawsar@capgemini.com>
2025-11-21 15:56:45 +0530

    add backend file and update yaml

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3"{
     bucket         = "my-terraform-bucket-ishika-2025-rn12capg"
     key            = "terraform/state.tfstate"
-    region         = "us-eash-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
