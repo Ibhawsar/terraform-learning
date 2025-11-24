4327d3dedd25d99ca252dff18a62a4a28ec6a7f6
Ishika1104 <ishika.bhawsar@capgemini.com>
2025-11-24 15:55:27 +0530

    changes made

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3"{
-    bucket         = "my-terraform-bucket-ishika-capgemini"
-    key            = "terraform/state.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-state"
-  }
-}
